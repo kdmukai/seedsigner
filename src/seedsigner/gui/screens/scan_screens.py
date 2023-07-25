27313e9d9ee3bdcda04adc65081931929560abac
kdmukai <kdmukai@gmail.com>
2023-07-24 20:40:06 -0500

    Update scan_screens.py

--- src/seedsigner/gui/screens/scan_screens.py
+++ src/seedsigner/gui/screens/scan_screens.py
@@ -54,6 +54,8 @@ class ScanScreen(BaseScreen):
             self.render_width = self.render_rect[2] - self.render_rect[0]
             self.render_height = self.render_rect[3] - self.render_rect[1]
 
+            self.decoder_fps = "0.0"
+
             print(f"render_width: {self.render_width}")
             print(f"render_height: {self.render_height}")
 
@@ -61,16 +63,24 @@ class ScanScreen(BaseScreen):
 
 
         def run(self):
-            from timeit import default_timer as timer
-
+            start_time = time.time()
+            num_frames = 0
+            show_framerate = True  # enable for debugging / testing
             instructions_font = Fonts.get_font(GUIConstants.BODY_FONT_NAME, GUIConstants.BUTTON_FONT_SIZE)
             while self.keep_running:
-                start = timer()
                 frame = self.camera.read_video_stream(as_image=True)
                 if frame is not None:
+                    num_frames += 1
+                    cur_time = time.time()
+                    cur_fps = num_frames / (cur_time - start_time)
                     scan_text = self.instructions_text
                     if self.decoder and self.decoder.get_percent_complete() > 0 and self.decoder.is_psbt:
                         scan_text = str(self.decoder.get_percent_complete()) + "% Complete"
+
+                    if show_framerate:
+                            scan_text = f" {cur_fps:0.2f} | {self.decoder_fps}"
+                    else:
+                        scan_text = self.instructions_text
 
                     with self.renderer.lock:
                         if frame.width > self.render_width or frame.height > self.render_height:
@@ -98,8 +108,7 @@ class ScanScreen(BaseScreen):
 
                         self.renderer.show_image()
 
-                        end = timer()
-                        # print(f"{1.0/(end - start)} fps") # Time in seconds, e.g. 5.38091952400282
+                    print(f" {cur_fps:0.2f} | {self.decoder_fps}")
 
                 time.sleep(0.05) # turn this up or down to tune performance while decoding psbt
                 if self.camera._video_stream is None:
@@ -112,11 +121,17 @@ class ScanScreen(BaseScreen):
             Screen. Once interaction starts, the display updates have to be managed in
             _run(). The live preview is an extra-complex case.
         """
+        num_frames = 0
+        start_time = time.time()
         while True:
             frame = self.camera.read_video_stream()
             if frame is not None:
                 status = self.decoder.add_image(frame)
 
+                num_frames += 1
+                decoder_fps = f"{num_frames / (time.time() - start_time):0.2f}"
+                self.threads[0].decoder_fps = decoder_fps
+                
                 if status in (DecodeQRStatus.COMPLETE, DecodeQRStatus.INVALID):
                     self.camera.stop_video_stream_mode()
                     break
